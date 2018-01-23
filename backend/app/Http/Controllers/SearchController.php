4a24d9f1ee2f638b6e5571f51c1911bfbb01516a
aloqui <al.oquindooff@gmail.com>
2018-01-24 02:41:45 +0800

    search

--- backend/app/Http/Controllers/SearchController.php
+++ backend/app/Http/Controllers/SearchController.php
@@ -10,10 +10,10 @@ class SearchController extends Controller
 {
     //
     public function show() {
-        $search = request('q');
 
-        $threads = Thread::search($search)->paginate(25);
-
+        if (request()->expectsJson()) {
+            return Thread::search(request('q'))->paginate(25);
+        }
         
         return $threads->load('channel', 'owner', 'replies');
         
@@ -21,11 +21,11 @@ class SearchController extends Controller
         // return something
     }
     public function showChannel() {
-        $search = request('q');
-
-        $channels = Channel::search($search)->paginate(5);
-
-        
+        // $search = request('q');
+        // $channels = Channel::search($search)->paginate(5);   
+        if (request()->expectsJson()) {
+            return Channel::search(request('q'))->paginate(25);
+        }     
         return $channels;
         
 
