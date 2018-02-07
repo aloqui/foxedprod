97a06c3d036009b1e7e14202d97f743f95836319
aloqui <al.oquindooff@gmail.com>
2018-02-08 02:11:12 +0800

    fixed icon

--- backend/app/Http/Controllers/Auth/ResetPasswordController.php
+++ backend/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -10,9 +10,12 @@ use App\User;
 class ResetPasswordController extends Controller
 {
 
-    protected function index(User $userData) {
-        $userData = User::where('email', request('email'))->firstOrFail();
-        $user = $userData->reset_password_token = str_limit(md5(request('email') . str_random()), 12, '');
+    protected function index(User $userData, Request $request) {
+        $this->validate($request, [
+            'email' => 'required'
+            ]);
+        $userData = User::where('email', $request['email'])->firstOrFail();
+        $user = $userData->reset_password_token = str_limit(md5($request['email'] . str_random()), 12, '');
         $userData->save();
         event(new ResetPassword($userData));
         }
