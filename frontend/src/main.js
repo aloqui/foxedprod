5fb76df49e94686a637482a1de2ff1fe3fd516fe
aloqui <al.oquindooff@gmail.com>
2018-02-18 12:18:10 +0800

    socket

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -22,6 +22,7 @@ Vue.use(InstantSearch)
 Vue.use(VueObserveVisibility)
 // Vue.use(VueSocketIo, `${location.host}/socket`);
 Vue.use(VueSocketIo, `${location.protocol}//${location.hostname}:8081`);
+Vue.use(VueSocketIo, `http://localhost:8081`);
 // Vue.use('/api/*', proxy({target: 'http://localhost:8000', secure: false, changeOrigin: true}))
 
 
